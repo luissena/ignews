a46b346f21792ff7ecba0b7292bdb02a000b4900
luissena <luis.senarode@gmail.com>
2022-09-21 15:09:31 -0300

    add header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,17 +6,13 @@ export default class MyDocumenta extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            
-          ></link>
+          <link rel="preconnect" href="https://fonts.gstatic.com"></link>
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           ></link>
 
-          
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
